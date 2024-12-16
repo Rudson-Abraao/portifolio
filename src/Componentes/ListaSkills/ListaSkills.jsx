d34776701ecd1047f9335fc7f4f2c5acb9370211
rudson <rudson.a.s.silva@gmail.com>
2024-12-16 12:06:02 -0300

    adicionado legenda nas skills

--- src/Componentes/ListaSkills/ListaSkills.jsx
+++ src/Componentes/ListaSkills/ListaSkills.jsx
@@ -3,14 +3,14 @@ import Skills from "../Skills/Skills";
 const ListaSkills = () => {
     return(
         <div className="flex gap-4 flex-wrap lg:max-w-[70%] m-auto flex justify-center">
-            <Skills imagem={"/imagens/icoJavascript.svg"}/>
-            <Skills imagem={"/imagens/icoReact.svg"}/>
-            <Skills imagem={"/imagens/icoJava.svg"}/>
-            <Skills imagem={"/imagens/icoPostgresql.svg"}/>
+            <Skills imagem={"/imagens/icoJavascript.svg"} linguagem={"JavaScript"}/>
+            <Skills imagem={"/imagens/icoReact.svg"} linguagem={"React Js"}/>
+            <Skills imagem={"/imagens/icoJava.svg"} linguagem={"Java"}/>
+            <Skills imagem={"/imagens/icoPostgresql.svg"} linguagem={"PostgreSql"}/>
             {/* <Skills imagem={"/imagens/icoSpringBoot.svg"}/> */}
-            <Skills imagem={"/imagens/icoHtml.svg"}/>
-            <Skills imagem={"/imagens/icoCss.svg"}/>
-            <Skills imagem={"/imagens/icoTailwind.svg"}/>
+            <Skills imagem={"/imagens/icoHtml.svg"} linguagem={"Html"}/>
+            <Skills imagem={"/imagens/icoCss.svg"} linguagem={"Css"}/>
+            <Skills imagem={"/imagens/icoTailwind.svg"} linguagem={"TailWind"}/>
         </div>
     )
 }  
--- src/Componentes/Skills/Skills.jsx
+++ src/Componentes/Skills/Skills.jsx
@@ -1,7 +1,8 @@
-const Skills = ({imagem}) => {
+const Skills = ({imagem, linguagem}) => {
     return (
-        <div className="h-[90px] w-[90px] sm:w-[150px] sm:h-[150px] p-4 sm:p-2 bg-cinzaClaro border-borda border flex items-center justify-center rounded-md">
+        <div className="h-[90px] w-[90px] sm:w-[150px] sm:h-[150px] p-4 sm:p-2 bg-cinzaClaro border-borda border flex items-center justify-center rounded-md flex-col gap-2">
             <img src={imagem} className="h-[80px] w-[80px]" />
+            <p className="text-cinza text-center hidden md:block ">{linguagem}</p>
         </div>    
     )
 }
